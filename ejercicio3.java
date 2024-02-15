021754432c7338a2fe1b83615bb2e9b7e97fb878
Alvaropernett <159142882+Alvaropernett@users.noreply.github.com>
2024-02-15 13:54:03 -0500

    Ejercicio semestre 2

---
+++ ejercicio3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int base,altura,area;
+        System.out.println("Digite la base del triangulo");
+        base = entrada.nextInt();
+        System.out.println("Digite la altura");
+        altura = entrada.nextInt();
+        area = (base * altura )/2;
+        System.out.println("El area del triangulo es:"+" "+area);        
+    }
+}
