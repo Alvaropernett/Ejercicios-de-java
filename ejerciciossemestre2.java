44f51bca73b3f95226f0cd8144da1fc4428e74b1
Alvaropernett <159142882+Alvaropernett@users.noreply.github.com>
2024-02-15 12:48:00 -0500

    Prueba

---
+++ ejerciciossemestre2.java
@@ -0,0 +1,9 @@
+public class ejerciciossemestre2{
+	public static void main(String[] args) {
+        System.out.println("**************************");
+
+        System.out.println("Fundamentos De Programacion");
+
+        System.out.println("**************************");
+}
+}
