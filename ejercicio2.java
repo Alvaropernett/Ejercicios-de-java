a3eee56640ee93dc9f76f3fc6ebdcbe979557602
Alvaropernett <159142882+Alvaropernett@users.noreply.github.com>
2024-02-15 13:24:31 -0500

    ejercicios2

---
+++ ejercicio2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String texto;
+        System.out.println("ingrese un texto");
+         texto = entrada.next();
+        System.out.println("el texto ingresado es:" + texto);
+    }
+}
