d37491631b7cecfbc4ae844f835330393d4901a5
Alvaropernett <159142882+Alvaropernett@users.noreply.github.com>
2024-02-29 16:57:06 -0500

    ejercicio16

---
+++ ejercicio16.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ejercicio16 {
+    public static void main(String[]args){
+        int suma = 0;
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+            suma +=i;
+        }
+        System.out.println("la suma de los numeros del 1 al 10 es:"+suma);
+    }
+    
+}
